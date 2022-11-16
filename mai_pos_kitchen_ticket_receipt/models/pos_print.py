c66c7d5c4d1287e147826f9d390c837b71220e4c
victorfaria9 <85259400+victorfaria9@users.noreply.github.com>
2022-11-16 16:08:27 -0400

    pos kitchen receipt

---
+++ mai_pos_kitchen_ticket_receipt/models/pos_print.py
@@ -0,0 +1,8 @@
+
+from odoo import fields, models,tools,api
+import logging
+
+class pos_config(models.Model):
+    _inherit = 'pos.config' 
+
+    allow_kitchen_ticket_print = fields.Boolean('Allow kitchen ticket print',default=True)
