6ba0bb3273c83983d4539dc70462a95f4069f5c8
victorfaria9 <85259400+victorfaria9@users.noreply.github.com>
2022-11-19 08:23:29 -0400

    customised KOT 2

--- mai_pos_customised_KOT/static/src/js/models.js
+++ mai_pos_customised_KOT/static/src/js/models.js
@@ -8,6 +8,7 @@ odoo.define('mai_pos_customised_KOT.models', function(require) {
 	var _t = core._t;
 	var Printer = require('point_of_sale.Printer').Printer;
 	var QWeb = core.qweb;
+	var field_utils = require('web.field_utils');
 
 	var _super_order = models.Order.prototype;
 	models.Order = models.Order.extend({
@@ -16,11 +17,13 @@ odoo.define('mai_pos_customised_KOT.models', function(require) {
 			var printers = this.pos.printers;
 			let isPrintSuccessful = true;
 			let client = this.get_client().name;
-			let cashier = this.pos.get_cashier().name;
+			// let cashier = this.pos.get_cashier().name;
+			let cashier = this.pos.config.name;
 			for(var i = 0; i < printers.length; i++){
 				var changes = this.computeChanges(printers[i].config.product_categories_ids);
 				changes['client'] = client;
 				changes['cashier'] = cashier;
+				changes['date']  = field_utils.format.datetime( moment(new Date()), {}, {timezone: false});
 				if ( changes['new'].length > 0 || changes['cancelled'].length > 0){
 					var receipt = QWeb.render('OrderChangeReceipt',{changes:changes, widget:this});
 					const result = await printers[i].print_receipt(receipt);
