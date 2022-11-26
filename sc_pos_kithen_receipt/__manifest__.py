7487d128a50ecd3eda471c2d97e9cbb64a2392e0
victorfaria9 <85259400+victorfaria9@users.noreply.github.com>
2022-11-26 17:53:16 -0400

    sc post kitchen receipt

---
+++ sc_pos_kithen_receipt/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': 'POS Kitchen Receipt | POS Custom Kitchen Receipt',
+    "description": """Using this module you can print POS Kitchen Receipt From front end""",
+    'summary': 'Using this module you can print POS Kitchen Receipt From front end',
+    'category': 'Point of Sale',    
+    'version': '14.1.1.1',
+    'sequence': 1,
+    "author" : "DOTSPRIME",
+    "email": 'dotsprime@gmail.com',
+    'price': 10,
+    'currency': 'EUR',
+    'license': 'OPL-1', 
+    "depends" : ['base','point_of_sale','pos_restaurant'],
+    "data": [
+        'views/pos_receipt_template.xml',
+        'views/pos_config_view.xml'
+    ],
+    'demo': [],
+    'images': ['static/description/main_screenshot.jpg'],  
+    "live_test_url" : "https://youtu.be/26pi7Y0NDsY",       
+    'qweb': ['static/src/xml/templates.xml'],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
